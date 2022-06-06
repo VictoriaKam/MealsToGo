97a94aacf6b8deff0770a08a369e7a9ce56cd8ef
Viktoriia Kamorina <kamorinavika@gmail.com>
2022-06-06 22:34:39 +0200

    Add restaurant card component.

---
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -0,0 +1,36 @@
+import React from "react";
+import styled from "styled-components/native";
+import { Card } from "react-native-paper";
+
+const RestaurantCard = styled(Card)`
+  background-color: white;
+`;
+
+const RestaurantCardCover = styled(Card.Cover)`
+  padding: 10px;
+  background-color: white;
+`;
+
+const Title = styled.Text`
+  padding: 8px;
+`;
+
+export const RestaurantInfoCard = ({ restaurant = {} }) => {
+  const {
+    name = "Some Restaurant",
+    icon,
+    photos = [
+      "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
+    ],
+    address = "200 Green Street",
+    isOpenNow = true,
+    rating = 4,
+    isClosedTemporarily,
+  } = restaurant;
+  return (
+    <RestaurantCard elevation={5}>
+      <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
+      <Title>{name}</Title>
+    </RestaurantCard>
+  );
+};
