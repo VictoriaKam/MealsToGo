8c3ce0812d4f627505420ec7cfcb1928c5729f16
Viktoriia Kamorina <kamorinavika@gmail.com>
2022-06-12 16:45:16 +0200

    Add map callout.

--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -5,6 +5,7 @@ import styled from "styled-components/native";
 import { LocationContext } from "../../../services/location/location.context";
 import { RestaurantsContext } from "../../../services/restaurants/restaurant.context";
 import { Search } from "../components/search.component";
+import { MapCallout } from "../components/map-callout.component";
 
 const Map = styled(MapView)`
   height: 100%;
@@ -46,7 +47,11 @@ export const MapScreen = () => {
                 latitude: restaurant.geometry.location.lat,
                 longitude: restaurant.geometry.location.lng,
               }}
-            />
+            >
+              <MapView.Callout>
+                <MapCallout restaurant={restaurant} />
+              </MapView.Callout>
+            </MapView.Marker>
           );
         })}
       </Map>
