f24ca4d79f2e9334a95c12072fc053302c6dd24f
Viktoriia Kamorina <kamorinavika@gmail.com>
2022-06-19 23:21:27 +0200

    Use payRequest function to set different states (loading, success, error).

--- src/features/checkout/components/credit-card.component.js
+++ src/features/checkout/components/credit-card.component.js
@@ -3,7 +3,7 @@ import { LiteCreditCardInput } from "react-native-credit-card-input";
 
 import { cardTokenRequest } from "../../../services/checkout/checkout.service";
 
-export const CreditCardInput = ({ name }) => {
+export const CreditCardInput = ({ name, onSuccess, onError }) => {
   const onChange = async (formData) => {
     const { values, status } = formData;
     const isIncomplete = Object.values(status).includes("incomplete");
@@ -17,8 +17,14 @@ export const CreditCardInput = ({ name }) => {
       name: name,
     };
 
-    const info = await cardTokenRequest(card);
-    console.log(info);
+    if (!isIncomplete) {
+      try {
+        const info = await cardTokenRequest(card);
+        onSuccess(info);
+      } catch (e) {
+        onError();
+      }
+    }
   };
   return <LiteCreditCardInput onChange={onChange} />;
 };
